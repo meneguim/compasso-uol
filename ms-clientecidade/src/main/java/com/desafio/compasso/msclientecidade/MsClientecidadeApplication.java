ac70db71e6b6c89cd474e0c1ae513ffc86452a97
Alan Meneguim <alan.meneguim@gmail.com>
2021-05-16 15:02:21 -0300

    Projeto de controle de cliente e cidade criado

---
+++ ms-clientecidade/src/main/java/com/desafio/compasso/msclientecidade/MsClientecidadeApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.compasso.msclientecidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientecidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientecidadeApplication.class, args);
+	}
+
+}
---
+++ ms-clientecidade/src/test/java/com/desafio/compasso/msclientecidade/MsClientecidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.compasso.msclientecidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientecidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
